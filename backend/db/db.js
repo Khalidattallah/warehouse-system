270a1f95a65221a72541fec7216400fbbc204b87
khalid <khalid@gmail.com>
2026-05-21 13:58:48 +0300

    connenct   modified:   backend/db/db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,15 +1,7 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  charset: "utf8mb4",
-});
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 pool
   .getConnection()
